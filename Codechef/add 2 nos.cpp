f5290679fd86552ffebba86170b60de17e95124e
Vikas Singh <44vikasrajput44@gmail.com>
2020-12-23 12:10:13 +0530

    Create add 2 nos.cpp

---
+++ Codechef/add 2 nos.cpp
@@ -0,0 +1,12 @@
+//Given two integers A and B, write a program to add these two numbers.
+
+int main(){
+int t;
+cin>>t;
+int a,b;
+for(int i=0; i<t;i++){
+cin>> a>>b;
+cout<<a+b<<endl;
+
+                      }
+           }
