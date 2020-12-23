1903644a18ca44bef94b45db7c00559ab4cea472
Vikas Singh <44vikasrajput44@gmail.com>
2020-12-23 18:26:05 +0530

    Create Comparing 2 numbers.cpp

---
+++ Codechef/Comparing 2 numbers.cpp
@@ -0,0 +1,12 @@
+//Comapare A and B
+// nested elseif
+
+int main(){
+int t,a,b; cin>>t;
+for (int i=0; i<t; i++){
+cin>>a>>b;
+if(a<b){ cout<<"<"<<endl;}
+elseif(a>b){cout<<">"<<endl;}
+else{cout<<"="<<endl;}
+}
+}
