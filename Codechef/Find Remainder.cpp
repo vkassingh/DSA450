b69f80888862fe5ddefdb67dfd0b2219cdc15b74
Vikas Singh <44vikasrajput44@gmail.com>
2020-12-23 12:36:36 +0530

    Create Find Remainder.cpp

---
+++ Codechef/Find Remainder.cpp
@@ -0,0 +1,17 @@
+//Write a program to find the remainder when an integer A is divided by an integer B.
+
+int main(){
+int t,a,b; cin t;
+for (int i=0; i<t ;i++)
+{
+cin>>a>>b;
+int rem= a%b;
+cout<<rem<<endl;
+
+
+
+}
+
+
+
+}
